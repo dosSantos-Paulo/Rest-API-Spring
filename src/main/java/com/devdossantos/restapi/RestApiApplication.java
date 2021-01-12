90f7670cbf65813e34f4e2556a6b1412d3edddaa
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2021-01-12 13:42:18 -0300

    criando projeto

---
+++ src/main/java/com/devdossantos/restapi/RestApiApplication.java
@@ -0,0 +1,13 @@
+package com.devdossantos.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiApplication.class, args);
+	}
+
+}
